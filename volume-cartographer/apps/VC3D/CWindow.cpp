8c0de2425d45cbfb2bf70b2f404975a3aca32400
SuperOptimizer <156155735+SuperOptimizer@users.noreply.github.com>
2025-08-11 07:54:11 -0500

    make chunk cache size configurable

--- volume-cartographer/apps/VC3D/CWindow.cpp
+++ volume-cartographer/apps/VC3D/CWindow.cpp
@@ -76,8 +76,8 @@ CWindow::CWindow() :
     ui.setupUi(this);
     // setAttribute(Qt::WA_DeleteOnClose);
 
-    //TODO make configurable
-    chunk_cache = new ChunkCache(10e9);
+    chunk_cache = new ChunkCache(CHUNK_CACHE_SIZE_GB*1024*1024*1024);
+    std::cout << "chunk cache size is " << CHUNK_CACHE_SIZE_GB << " gigabytes " << std::endl;
     
     _surf_col = new CSurfaceCollection();
     
