c1091b536f40644611f7752aff58e6acc1464228
SuperOptimizer <156155735+SuperOptimizer@users.noreply.github.com>
2025-08-23 14:13:47 -0500

    remove #pragma in readinterpolated3d that was causing compile errors

--- volume-cartographer/core/src/Slicing.cpp
+++ volume-cartographer/core/src/Slicing.cpp
@@ -349,7 +349,6 @@ void readInterpolated3D(cv::Mat_<uint8_t> &out, z5::Dataset *ds,
 
         }
 
-#pragma omp parallel for schedule(dynamic)
     for(auto &it : chunks) {
         xt::xarray<uint8_t> *chunk = nullptr;
         std::shared_ptr<xt::xarray<uint8_t>> chunk_ref;
