3fa8e81abc927fae717bf584706623fd3339a27a
Johannes Rudolph <johannes.rudolph@gmail.com>
2025-10-14 15:22:56 +0200

    inference: build separate smalelr image for CPU-only tasks

--- ink-detection/optimized_inference/build.sh
+++ ink-detection/optimized_inference/build.sh
@@ -4,5 +4,9 @@ VERSION=$1
 DIR=$(readlink -f $(dirname $0))
 
 docker build $DIR \
-    --progress plain --builder kube --platform linux/amd64 --push \
+    --progress plain --builder kube --platform linux/amd64 --target gpu --push \
     --tag 585768151128.dkr.ecr.us-east-1.amazonaws.com/scrollprize/ink-detection:$VERSION
+
+docker build $DIR \
+    --progress plain --builder kube --platform linux/amd64,linux/arm64 --target cpu --push \
+    --tag 585768151128.dkr.ecr.us-east-1.amazonaws.com/scrollprize/ink-detection:$VERSION-cpu
