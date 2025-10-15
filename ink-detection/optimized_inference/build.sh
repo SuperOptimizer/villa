f36e967063cce2f2e4df7f87620739f8e128693e
Johannes Rudolph <johannes.rudolph@gmail.com>
2025-10-15 12:01:58 +0200

    inference: support s3 urls as surface zarrs
    To avoid extra surface zarr preparation if already available

--- ink-detection/optimized_inference/build.sh
+++ ink-detection/optimized_inference/build.sh
@@ -10,5 +10,5 @@ docker build $DIR \
     --tag 585768151128.dkr.ecr.us-east-1.amazonaws.com/scrollprize/ink-detection:$VERSION
 
 docker build $DIR \
-    --progress plain --builder kube --platform linux/amd64 --target cpu --push \
+    --progress plain --builder kube --platform linux/amd64,linux/arm64 --target cpu --push \
     --tag 585768151128.dkr.ecr.us-east-1.amazonaws.com/scrollprize/ink-detection:$VERSION-cpu
