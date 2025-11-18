d59567e9294bef1d5fbd854acbe246e5d7f39c0f
SuperOptimizer <156155735+SuperOptimizer@users.noreply.github.com>
2025-11-18 14:26:37 +0000

    delete opchain stuff and the surfaces that are only used by it

--- volume-cartographer/apps/VC3D/CWindow.hpp
+++ volume-cartographer/apps/VC3D/CWindow.hpp
@@ -35,9 +35,6 @@
 #include "ViewerManager.hpp"
 #include "segmentation/SegmentationWidget.hpp"
 #include "segmentation/SegmentationGrowth.hpp"
-#include "OpChain.hpp"
-#include "OpsList.hpp"
-#include "OpsSettings.hpp"
 #include "SeedingWidget.hpp"
 #include "vc/core/types/Volume.hpp"
 #include "vc/core/types/VolumePkg.hpp"
@@ -76,7 +73,6 @@ public:
 
 signals:
     void sendVolumeChanged(std::shared_ptr<Volume> vol, const std::string& volumeId);
-    void sendOpChainSelected(OpChain*);
     void sendSurfacesLoaded();
     void sendVolumeClosing(); // Signal to notify viewers before closing volume
 
@@ -85,7 +81,6 @@ public slots:
     void onLocChanged(void);
     void onManualPlaneChanged(void);
     void onVolumeClicked(cv::Vec3f vol_loc, cv::Vec3f normal, Surface *surf, Qt::MouseButton buttons, Qt::KeyboardModifiers modifiers);
-    void onOpChainChanged(OpChain *chain);
     void onRenderSegment(const std::string& segmentId);
     void onGrowSegmentFromSegment(const std::string& segmentId);
     void onAddOverlap(const std::string& segmentId);
@@ -163,7 +158,7 @@ private slots:
     void configureViewerConnections(CVolumeViewer* viewer);
     CVolumeViewer* segmentationViewer() const;
     void clearSurfaceSelection();
-    void onSurfaceActivated(const QString& surfaceId, QuadSurface* surface, OpChain* chain);
+    void onSurfaceActivated(const QString& surfaceId, QuadSurface* surface);
     void onAxisAlignedSliceMousePress(CVolumeViewer* viewer, const cv::Vec3f& volLoc, Qt::MouseButton button, Qt::KeyboardModifiers modifiers);
     void onAxisAlignedSliceMouseMove(CVolumeViewer* viewer, const cv::Vec3f& volLoc, Qt::MouseButtons buttons, Qt::KeyboardModifiers modifiers);
     void onAxisAlignedSliceMouseRelease(CVolumeViewer* viewer, Qt::MouseButton button, Qt::KeyboardModifiers modifiers);
@@ -191,10 +186,8 @@ private:
     CPointCollectionWidget* _point_collection_widget;
 
     VCCollection* _point_collection;
-    
+
     SurfaceTreeWidget *treeWidgetSurfaces;
-    OpsList *wOpsList;
-    OpsSettings *wOpsSettings;
     QPushButton *btnReloadSurfaces;
     
     //TODO abstract these into separate QWidget class?
@@ -222,8 +215,6 @@ private:
     bool _useAxisAlignedSlices{false};
     bool _mirrorCursorToSegmentation{false};
     std::unique_ptr<SegmentationGrower> _segmentationGrower;
-
-    std::unordered_map<std::string, OpChain*> _opchains;
 
     std::unique_ptr<SegmentationEditManager> _segmentationEdit;
     std::unique_ptr<SegmentationOverlayController> _segmentationOverlay;
