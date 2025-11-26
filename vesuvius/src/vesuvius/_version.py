8076458f3a04b42259fd2be9c3445e20c8888379
Giorgio Angelotti <76100950+giorgioangel@users.noreply.github.com>
2025-11-26 20:29:38 +0100

    Update version vesuvius and add missing deps (#591)
    * version update
    
    * adding tests extra
    
    * workflow vesuvius test with all extras
    
    * adding back numba
    
    * other deps
    
    * add missing deps

--- vesuvius/src/vesuvius/_version.py
+++ vesuvius/src/vesuvius/_version.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 import os
 
-VERSION: str = os.environ.get("VERSION", "0.2.2")
+VERSION: str = os.environ.get("VERSION", "0.2.3")
 """Current package version, optionally overridden via the VERSION env var."""
 
 __all__ = ["VERSION"]
